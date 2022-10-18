f42a6d85084c94d6d02251376d719e7329601191
Quan Pham <doraemonfanclub@gmail.com>
2022-10-19 00:36:21 +0700

    Complete the app with UI

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
+#include "video_player.h"
 
 #include <QApplication>
+#include <QtWidgets/QDesktopWidget>
 #include <QSharedMemory>
 #include <QMessageBox>
+#include <QtCore/QCommandLineParser>
+#include <QtCore/QCommandLineOption>
+#include <QtCore/QDir>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +18,21 @@ int main(int argc, char *argv[])
     QGuiApplication::setApplicationDisplayName(QCoreApplication::applicationName());
     QCoreApplication::setApplicationVersion(QT_VERSION_STR);
 
+    QCommandLineParser parser;
+
+    parser.setApplicationDescription("Qt Video Widget Example");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("url", "The URL of video to open.");
+    parser.process(a);
+
+    VideoPlayer player;
+    if (!parser.positionalArguments().isEmpty())
+    {
+        const QUrl url = QUrl::fromUserInput(parser.positionalArguments().constFirst(), QDir::currentPath(), QUrl::AssumeLocalFile);
+        player.setUrl(url);
+    }
+
     // https://stackoverflow.com/questions/34445164/qt5-preventing-another-instance-of-the-application-doesnt-work-anymore
     sharedMemory.setKey("fyi-memory-key");
     if (sharedMemory.create(1) == false)
@@ -22,8 +42,9 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    MainWindow w;
-    w.resize(800, 600);
-    w.show();
-    return a.exec();
+    const QRect availableSpace = QApplication::desktop()->availableGeometry(&player);
+    player.resize(availableSpace.width() * 0.5, availableSpace.height() / 3);
+    player.show();
+
+     return a.exec();
 }
--- src/video_player.cpp
+++ src/video_player.cpp
@@ -1,14 +1,128 @@
-#ifndef VIDEOPLAYER_H
-#define VIDEOPLAYER_H
 
+#include "video_player.h"
+
+#include <QtWidgets>
 #include <QVideoWidget>
-#include <QSet>
 
-QT_BEGIN_NAMESPACE
-class QAbstractButton;
-class QSlider;
-class QLabel;
-class QUrl;
-QT_END_NAMESPACE
+VideoPlayer::VideoPlayer(QWidget *parent) : QWidget(parent)
+{
+  m_mediaPlayer = new QMediaPlayer(this, QMediaPlayer::VideoSurface);
+  QVideoWidget *videoWidget = new QVideoWidget();
 
-#endif
+  QAbstractButton *openButton = new QPushButton(tr("Open file..."));
+  connect(openButton, &QAbstractButton::clicked, this, &VideoPlayer::openFile);
+
+  m_playButton = new QPushButton();
+  m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+  m_playButton->setEnabled(false);
+
+  connect(m_playButton, &QAbstractButton::clicked, this, &VideoPlayer::play);
+
+  m_positionSlider = new QSlider(Qt::Horizontal);
+  m_positionSlider->setRange(0, 0);
+
+  connect(m_positionSlider, &QAbstractSlider::sliderMoved, this, &VideoPlayer::setPosition);
+
+  m_errorLabel = new QLabel();
+  m_errorLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
+
+  QBoxLayout *controlLayout = new QHBoxLayout;
+  controlLayout->setContentsMargins(0, 0, 0, 0);
+  controlLayout->addWidget(m_playButton);
+  controlLayout->addWidget(openButton);
+  controlLayout->addWidget(m_positionSlider);
+
+  QVBoxLayout *masterLayout = new QVBoxLayout;
+  masterLayout->addWidget(videoWidget);
+  masterLayout->addWidget(m_errorLabel);
+  masterLayout->addLayout(controlLayout);
+
+  setLayout(masterLayout);
+
+  m_mediaPlayer->setVideoOutput(videoWidget);
+  connect(m_mediaPlayer, &QMediaPlayer::stateChanged, this, &VideoPlayer::mediaStateChanged);
+  connect(m_mediaPlayer, &QMediaPlayer::positionChanged, this, &VideoPlayer::positionChanged);
+  connect(m_mediaPlayer, &QMediaPlayer::durationChanged, this, &VideoPlayer::durationChanged);
+  connect(m_mediaPlayer, QOverload<QMediaPlayer::Error>::of(&QMediaPlayer::error), this, &VideoPlayer::handleError);
+}
+
+VideoPlayer::~VideoPlayer()
+{
+}
+
+void VideoPlayer::openFile()
+{
+  QFileDialog fileDialog; // TODO
+  fileDialog.setAcceptMode(QFileDialog::AcceptOpen);
+  fileDialog.setWindowTitle("Open video file");
+  QStringList supportedMimeTypes = m_mediaPlayer->supportedMimeTypes();
+  if (!supportedMimeTypes.isEmpty())
+  {
+    fileDialog.setMimeTypeFilters(supportedMimeTypes);
+  }
+  fileDialog.setDirectory(QStandardPaths::standardLocations(QStandardPaths::MoviesLocation).value(0, QDir::homePath()));
+  if (fileDialog.exec() == QDialog::Accepted)
+  {
+    setUrl(fileDialog.selectedUrls().constFirst());
+  }
+}
+
+void VideoPlayer::setUrl(const QUrl &url)
+{
+  m_errorLabel->setText(QString());
+  setWindowFilePath(url.isLocalFile() ? url.toLocalFile() : QString());
+  m_mediaPlayer->setMedia(url);
+  m_playButton->setEnabled(true);
+}
+
+void VideoPlayer::play()
+{
+  switch (m_mediaPlayer->state())
+  {
+  case QMediaPlayer::PlayingState:
+    m_mediaPlayer->pause();
+    break;
+
+  default:
+    m_mediaPlayer->play();
+    break;
+  }
+}
+
+void VideoPlayer::mediaStateChanged(QMediaPlayer::State state)
+{
+  if (state == QMediaPlayer::PlayingState)
+  {
+    m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+    return;
+  }
+  m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+  return;
+}
+
+void VideoPlayer::positionChanged(qint64 position)
+{
+  m_positionSlider->setValue(position);
+}
+
+void VideoPlayer::durationChanged(qint64 duration)
+{
+  m_positionSlider->setRange(0, duration);
+}
+
+void VideoPlayer::setPosition(int position)
+{
+  m_mediaPlayer->setPosition(position);
+}
+
+void VideoPlayer::handleError()
+{
+  m_playButton->setEnabled(false);
+  const QString errorString = m_mediaPlayer->errorString();
+  QString message = "Error: ";
+  if (errorString.isEmpty())
+    message += " #" + QString::number(int(m_mediaPlayer->error()));
+  else
+    message += errorString;
+  m_errorLabel->setText(message);
+}
